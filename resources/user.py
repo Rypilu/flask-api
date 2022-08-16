ba8b21e6067369b183f002c40b7eaf95396c3aa4
rypilu <rpiluski@gmail.com>
2022-08-16 07:43:26 -0400

    implemented token refresh endpoint and customized Flask-JWT-Extended callbacks and responses

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,13 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity
+)
 from models.user_model import UserModel
+
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -58,3 +64,10 @@ class UserLogin(Resource):
             }, 200
 
         return {'message': 'Invalid credentials'}, 401
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'acceses_token': new_token}, 200
